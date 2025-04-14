52b56d0b61baea3ca696e6a1260045940c4ac901
yakimovaan <151525632+yakimovaan@users.noreply.github.com>
2025-04-14 09:41:13 +0300

    locators

---
+++ pages/LoginPage.py
@@ -0,0 +1,17 @@
+from pages.BasePage import BasePage
+from selenium.webdriver.common.by import By
+
+class LoginPageLocators:
+    LOGIN_FIELD = (By.ID, "field_email")
+    PASSWORD_FIELD = (By.ID, "field_password")
+    LOGIN_BUTTON = (By.XPATH, "//*[@data-l='t,sign_in']")
+    QR_LOGIN_BUTTON = (By.XPATH, "//*[@data-l='t,get_qr']")
+    QR_CODE_BUTTON = (By.XPATH, "//*[@data-l='t,qr_tab']")
+    DOESNT_WORK_BUTTON = (By.XPATH, "//*[@data-l='t,restore']")
+    REGISTRATION_BUTTON = (By.XPATH, "//*[@class='button-pro __sec mb-3x __wide']")
+    VK_BUTTON = (By.XPATH, "//*[@class='i ic social-icon __s __vk_id']")
+    MAIL_BUTTON = (By.XPATH, "//*[@class='i ic social-icon __s __mailru']")
+    YANDEX_BUTTON = (By.XPATH, "//*[@class='i ic social-icon __s __yandex']")
+
+class LoginPageHelper(BasePage):
+    pass
