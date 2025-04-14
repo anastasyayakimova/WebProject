c33477d01ceb33f42a57d23b5eb0b6b0b2c13724
yakimovaan <151525632+yakimovaan@users.noreply.github.com>
2025-04-14 15:55:54 +0300

    for

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -10,11 +10,14 @@ class LoginPageLocators:
     QR_LOGIN_BUTTON = (By.XPATH, "//*[@data-l='t,get_qr']")
     QR_CODE_BUTTON = (By.XPATH, "//*[@data-l='t,qr_tab']")
     DOESNT_WORK_BUTTON = (By.XPATH, "//*[@data-l='t,restore']")
+    RESTORE_LINK = (By.XPATH, "//div[@class='form-actions']/*[@data-l='t,restore']")
     REGISTRATION_BUTTON = (By.XPATH, "//div[@class='external-oauth-login-footer']//a[@data-l='t,register']")
     VK_BUTTON = (By.XPATH, "//*[@class='i ic social-icon __s __vk_id']")
     MAIL_BUTTON = (By.XPATH, "//*[@class='i ic social-icon __s __mailru']")
     YANDEX_BUTTON = (By.XPATH, "//*[@class='i ic social-icon __s __yandex']")
     ERROR_TEXT = (By.XPATH, "//*[@class='input-e login_error']")
+    GO_BACK_BUTTON = (By.XPATH, "//*[@data-l='t,cancel']")
+    SUPPORT_BUTTON = (By.XPATH, "//*[text()='Служба поддержки']")
 
 class LoginPageHelper(BasePage):
     def __init__(self, driver):
@@ -22,6 +25,8 @@ class LoginPageHelper(BasePage):
         self.check_page()
 
     def check_page(self):
+        with allure.step("Проверить корректность загрузки страницы"):
+            self.attach_screenshot()
         self.find_element(LoginPageLocators.LOGIN_FIELD)
         self.find_element(LoginPageLocators.PASSWORD_FIELD)
         self.find_element(LoginPageLocators.LOGIN_BUTTON)
@@ -41,8 +46,19 @@ class LoginPageHelper(BasePage):
     @allure.step("Заполнить поле Логин")
     def send_keys_login(self, text):
         self.find_element(LoginPageLocators.LOGIN_FIELD).send_keys(text)
+        self.attach_screenshot()
+
+    @allure.step("Заполнить поле Пароль")
+    def send_keys_password(self, text):
+        self.find_element(LoginPageLocators.PASSWORD_FIELD).send_keys(text)
+        self.attach_screenshot()
 
     @allure.step("Получить текст ошибки")
     def get_error_text(self):
         self.attach_screenshot()
         return self.find_element(LoginPageLocators.ERROR_TEXT).text
+
+    @allure.step("Кликнуть на кнопку Восстановить профиль")
+    def click_recovery(self):
+        self.attach_screenshot()
+        self.find_element(LoginPageLocators.RESTORE_LINK).click()
---
+++ tests/test_recovery.py
@@ -0,0 +1,20 @@
+from core.BaseTest import browser
+from pages.BasePage import BasePage
+from pages.LoginPage import LoginPageHelper
+from pages.RecoveryPage import RecoveryPageHelper
+import allure
+
+BASE_URL = "https://ok.ru/"
+@allure.suite("Проверка восстановления пользователя")
+@allure.title("Проверка перехода к восстановлению после несколкьких неудачных попыток авторизации")
+def test_go_to_recovery_after_fails_login(browser):
+    BasePage(browser).get_url(BASE_URL)
+    LoginPage = LoginPageHelper(browser)
+    LoginPage.send_keys_login("test")
+
+    for i in range(3):
+        LoginPage.send_keys_password("1")
+        LoginPage.click_login()
+
+    LoginPage.click_recovery()
+    RecoveryPageHelper(browser)
