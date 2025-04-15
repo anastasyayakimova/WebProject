56d190756b5b96b41e8e721c344a28794b443bfc
yakimovaan <151525632+yakimovaan@users.noreply.github.com>
2025-04-15 08:15:56 +0300

    регистрация

--- pages/RegistrationPage.py
+++ pages/RegistrationPage.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 
 class RegistrationPageLocators:
     COUNTRY_LIST = (By.XPATH, "//div[@data-l='t,country']")
-    COUNTRY_ITEM = (By.XPATH, "//*[@class='country-select_i']")
+    COUNTRY_ITEM = (By.XPATH, "//div[@class='country-select_code']")
     PHONE_FIELD = (By.XPATH, "//*[@id='field_phone']")
     BUTTON_FURTHER = (By.XPATH, "//input[@value='Далее']")
     SUPPORT_BUTTON = (By.XPATH, "//*[@class='ext-registration_f-support-link']")
@@ -23,8 +23,16 @@ class RegistrationPageHelper(BasePage):
         self.find_element(RegistrationPageLocators.SUPPORT_BUTTON)
         self.find_element(RegistrationPageLocators.BUTTON_FURTHER)
 
-
+    @allure.step("Выбрать рандомную страну из списка")
     def select_random_country(self):
         random_number = random.randint(0, 112)
         self.find_element(RegistrationPageLocators.COUNTRY_LIST).click()
         country_items = self.find_elements(RegistrationPageLocators.COUNTRY_ITEM)
+        country_code = country_items[random_number].text
+        country_items[random_number].click()
+        self.attach_screenshot()
+        return country_code
+
+    @allure.step('Получить ззначение поля Телефон')
+    def get_phone_field_value(self):
+        return self.find_element(RegistrationPageLocators.PHONE_FIELD).get_attribute('value')
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -6,9 +6,13 @@ import allure
 
 BASE_URL = "https://ok.ru/"
 
+@allure.suite("Проверка Регитрации")
+@allure.title("Проверка выбора рандомного элемента из выпадающего списка")
 def test_registration_random_country(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
     LoginPage.click_registration()
     RegistrationPage = RegistrationPageHelper(browser)
-    RegistrationPage.select_random_country()
+    Selected_country_code = RegistrationPage.select_random_country()
+    Actual_country_code = RegistrationPage.get_phone_field_value()
+    assert Selected_country_code == Actual_country_code
